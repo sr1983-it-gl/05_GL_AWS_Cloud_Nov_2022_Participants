889514fdd4f4cc9ce4021367694a9b96a3a7231c
pshunmugaraja1983 <pshunmugaraja1983@gmail.com>
2023-11-05 03:22:25 +0530

    Files

---
+++ Week-06/RDS-Multi-AZ-Demo/src/rds/multiaz/demo/ApplicationConstants.java
@@ -0,0 +1,21 @@
+package rds.multiaz.demo;
+
+public class ApplicationConstants {
+
+	public static String DRIVER_CLASS_NAME 
+		= "com.mysql.cj.jdbc.Driver";
+
+	public static String HOST = 
+			
+"rds-mysql-multi-az-instance-01.cnq8huiucjab.us-east-1.rds.amazonaws.com";
+	
+	public static String DB_NAME = "mydb";
+	
+	public static String USERNAME = "admin";
+	
+	public static String PASSWORD = "password";
+	
+	public static String CONNECTION_URL
+		=	"jdbc:mysql://" + HOST + ":3306/" + DB_NAME;
+	
+}
